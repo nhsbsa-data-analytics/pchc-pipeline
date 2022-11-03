651b948ebd73fa5c40f13275af22e41bb51c0ae5
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-11-03 11:12:12 +0000

    Pipeline (#8)
    * Build table_1a
    
    * Build table_3
    
    * Build table_2 and table_4
    
    * table_5 build
    
    * table_6 build
    
    * Build table_7
    
    * Update pipeline with new table names and headers
    
    * Build Excel tables
    
    * Complete narrative updates
    
    * Add markdown names
    
    * Delete pipeline_old
    
    * Set out.widths to 100%
    
    * Update key findings
    
    * Updates following GL SPAG check
    
    * Updates following KG QR

--- pipeline.R
+++ pipeline.R
@@ -46,15 +46,14 @@ con <- con_nhsbsa(
 # 4. extract data tables from fact table -----------------------------------------
 
 #dwh tables
-table_1_dwh <- table_1_dwh(con) |> filter(`Financial Year` != "2016/2017")
-table_2_dwh <- table_2_dwh(con) |> filter(`Financial Year` != "2016/2017")
-table_3_dwh <- table_3_dwh(con) |> filter(FINANCIAL_YEAR != "2016/2017",
-                                          FINANCIAL_YEAR != "2017/2018")
-table_4_dwh <- table_4_dwh(con) |> filter(`Year Month` >= 201704)
-table_5_dwh <- table_5_dwh(con) |> filter(`Financial Year` != "2016/2017")
-table_6_dwh <- table_6_dwh(con) |> filter(`Financial Year` != "2016/2017")
-table_7_dwh <- table_7_dwh(con) |> filter(`Financial Year` != "2016/2017")
-table_8_dwh <- table_8_dwh(con) |> filter(`Financial Year` != "2016/2017")
+table_1_dwh <- table_1_dwh(con)
+table_2_dwh <- table_2_dwh(con)
+table_3_dwh <- table_3_dwh(con)
+table_4_dwh <- table_4_dwh(con)
+table_5_dwh <- table_5_dwh(con)
+table_6_dwh <- table_6_dwh(con)
+table_7_dwh <- table_7_dwh(con)
+table_8_dwh <- table_8_dwh(con)
 
 
 # disconnect from DWH
@@ -713,8 +712,8 @@ meta_fields <- c(
    "Hospital prescribing issued within hospitals (GBP)",
    "Population",
    "Primary care prescribing dispensed in the community (GBP)",
-   "ICB",
    "ICB Code",
+   "ICB",
    "Undefined data"
 )
 
@@ -747,7 +746,7 @@ write_sheet(
   "Table_1",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 1: Total costs in each setting by financial year"
+    "Table 1: Total costs in each setting by financial year using actual costs in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -779,7 +778,7 @@ write_sheet(
   "Table_2",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 2: Total costs in each setting by financial year"
+    "Table 2: Total costs in each setting by financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -811,7 +810,7 @@ write_sheet(
   "Table_3",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 3: Percentage change in costs in each setting by financial year"
+    "Table 3: Percentage change in costs in each setting by financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -843,7 +842,7 @@ write_sheet(
   "Table_4",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 4: Total costs in each setting by month, April 2017 to March 2022"
+    "Table 4: Total costs in each setting by month, April 2017 to March 2022 using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -875,7 +874,7 @@ write_sheet(
   "Table_5",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 5: Total costs in each setting by BNF Chapter and financial year"
+    "Table 5: Total costs in each setting by BNF Chapter and financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -907,7 +906,7 @@ write_sheet(
   "Table_6",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 6: Total costs in each setting by BNF Section and financial year"
+    "Table 6: Total costs in each setting by BNF Section and financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -939,7 +938,7 @@ write_sheet(
   "Table_7",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 7: Total costs in each setting by Integrated Care Board (ICB) and financial year"
+    "Table 7: Total costs in each setting by Integrated Care Board (ICB) and financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -978,7 +977,7 @@ write_sheet(
   "Table_8",
   paste0(
     "Prescribing Costs in Hospitals and the Community - England 2017/18 - 2021/22 - ",
-    "Table 8: Total costs in each setting by BNF Section, ICB and financial year"
+    "Table 8: Total costs in each setting by BNF Section, ICB and financial year using Net Ingredient Costs (NIC) in primary care"
   ),
   c(
     "1. Field definitions can be found on the 'Metadata' tab.",
@@ -1014,24 +1013,22 @@ openxlsx::saveWorkbook(wb,
 
 # 8. render markdowns ------------------------------------------------------
 
-rmarkdown::render("hrt-narrative.Rmd",
+rmarkdown::render("pres-costs-hosp-comm-v1.Rmd",
                   output_format = "html_document",
-                  output_file = paste0("outputs/hrt_",
-                                       gsub(" ", "_", ltst_month_tidy),
-                                       "_v001.html"))
+                  output_file = "outputs/pchc-2021-2022-narrative-v001.html")
 
 
-rmarkdown::render("hrt-narrative.Rmd",
+rmarkdown::render("pres-costs-hosp-comm-v1.Rmd",
                   output_format = "word_document",
-                  output_file = paste0("outputs/hrt_",
-                                       gsub(" ", "_", ltst_month_tidy),
-                                       "_v001.docx"))
+                  output_file = "outputs/pchc-2021-2022-narrative-v001.docx")
 
-rmarkdown::render("hrt-background.Rmd",
+rmarkdown::render("pchc-background.Rmd",
                   output_format = "html_document",
-                  output_file = "outputs/hrt-background-info-methodology-v001.html")
+                  output_file = "outputs/pchc_2022_background_information_methodology_v001.html")
 
-rmarkdown::render("hrt-background.Rmd",
+
+rmarkdown::render("pchc-background.Rmd",
                   output_format = "word_document",
-                  output_file = "outputs/hrt-background-info-methodology-v001.docx")
+                  output_file = "outputs/pchc_2022_background_information_methodology_v001.docx")
+
 
